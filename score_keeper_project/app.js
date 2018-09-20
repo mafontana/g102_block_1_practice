70f8b9208e815cdfd8cb4fb94b3b0148b7272e52
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-09-19 18:37:28 -0600

    button for player two can change score

--- score_keeper_project/app.js
+++ score_keeper_project/app.js
@@ -1,8 +1,17 @@
 var playerOneButton = document.querySelector("#p1")
+var playerTwoButton = document.querySelector("#p2")
 var p1score = 0
+var p2score = 0
 var p1Display = document.querySelector("#p1Display")
+var p2Display = document.querySelector("#p2Display")
 
 playerOneButton.addEventListener("click", function() {
     p1score++; 
     p1Display.textContent = p1score
+})
+
+
+playerTwoButton.addEventListener("click", function () {
+    p2score++;
+    p2Display.textContent = p2score
 })
